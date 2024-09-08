00353ed19873a5d3e4f7da686d735484ca72951e
JuanjoLetelier <ju.letelier@duocuc.cl>
2024-09-08 12:10:54 -0300

    Se agrega proyecto

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+
+  firebaseConfig : {
+    apiKey: "AIzaSyAY2lJjY_8rgXXXn_0PgqOrzuDinHv0PRw",
+    authDomain: "isekaiapp.firebaseapp.com",
+    projectId: "isekaiapp",
+    storageBucket: "isekaiapp.appspot.com",
+    messagingSenderId: "515031612956",
+    appId: "1:515031612956:web:1e481264b943b8c82da9d9"
+  }
+};
